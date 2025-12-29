4b604bf1e184a7115f4f2304d1da5107eb969bfa
vishwakarmaanshika3981-bit <vishwakarmaanshika3981@gmail.com>
2025-12-29 15:36:32 +0530

    Update chat_app.py

--- chat_app.py
+++ chat_app.py
@@ -18,7 +18,7 @@ model = genai.GenerativeModel("gemini-2.5-flash")
 
 # Page config
 st.set_page_config(page_title="Gemini Chat", page_icon="🤖")
-st.title("🤖 Gemini Multi-Turn Chat")
+st.title("🤖 AI-CHATBOT")
 
 # Initialize chat history
 if "chat" not in st.session_state:
@@ -57,7 +57,7 @@ if user_prompt:
             for char in assistant_reply:
                 typed_text += char
                 placeholder.markdown(typed_text)
-                time.sleep(0.0002)  # typing speed (adjust)
+                time.sleep(0.02)  # typing speed (adjust)
 
     st.session_state.messages.append(
         {"role": "assistant", "content": assistant_reply}
@@ -68,6 +68,10 @@ if st.sidebar.button("🔄 Reset Conversation"):
     st.session_state.chat = model.start_chat(history=[])
     st.session_state.messages = []
     st.experimental_rerun()
+    
+    
+    st.markdown('**design and developed by: Anshika**')
+
 
 
 
